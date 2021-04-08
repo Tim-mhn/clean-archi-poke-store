ad1e0663c57c92b7f3ac8af4ad5f41e817581f17
AlexJerome <alexandra.jerome@student.ecp.fr>
2021-04-08 16:34:42 +0200

    feat(Store): add pokemon id in pokemon list of store

--- src/infrastructure/presenters/getQuantityOfPokemonAvailableInStoreAndPrice.presenter.ts
+++ src/infrastructure/presenters/getQuantityOfPokemonAvailableInStoreAndPrice.presenter.ts
@@ -1,38 +1,46 @@
 import { Service } from "typedi";
-import { Pokemon, PokemonType } from "../../domain/entities/pokemon.entity";
+import { PokemonType } from "../../domain/entities/pokemon.entity";
 import { GetQuantityOfPokemonAvailableInStoreAndPriceOutput } from "../../domain/usecases/getQuantityOfPokemonAvailableInStoreAndPrice.usecase";
 
-
 type PokemonInfo = {
-    name: string,
-    type: PokemonType,
-    weight: number
-}
+  id: string;
+  name: string;
+  type: PokemonType;
+  weight: number;
+};
 type AvailablePokemon = {
-    quantity: number,
-    unitPrice: number,
-    pokemon: PokemonInfo
-}
-type GetQuantityOfPokemonAvailableInStoreAndPricePresenterOutput = { storeId: string, availablePokemons: AvailablePokemon[]};
+  quantity: number;
+  unitPrice: number;
+  pokemon: PokemonInfo;
+};
+type GetQuantityOfPokemonAvailableInStoreAndPricePresenterOutput = {
+  storeId: string;
+  availablePokemons: AvailablePokemon[];
+};
 
 @Service()
 export class GetQuantityOfPokemonAvailableInStoreAndPricePresenter {
-    public present(useCaseOutput: GetQuantityOfPokemonAvailableInStoreAndPriceOutput): GetQuantityOfPokemonAvailableInStoreAndPricePresenterOutput {
-        const availablePokemons: AvailablePokemon[] = useCaseOutput.availablePokemonsWithPriceAndQuantity.map(availablePoke => {
-            return {
-                unitPrice: availablePoke.price,
-                quantity: availablePoke.quantity,
-                pokemon: {
-                    name: availablePoke.pokemon.name,
-                    type: availablePoke.pokemon.type,
-                    weight: availablePoke.pokemon.weight
-                }
-            }
-        });
-
+  public present(
+    useCaseOutput: GetQuantityOfPokemonAvailableInStoreAndPriceOutput
+  ): GetQuantityOfPokemonAvailableInStoreAndPricePresenterOutput {
+    const availablePokemons: AvailablePokemon[] = useCaseOutput.availablePokemonsWithPriceAndQuantity.map(
+      (availablePoke) => {
         return {
-            storeId: useCaseOutput.id,
-            availablePokemons
-        }
-    }
+          unitPrice: availablePoke.price,
+          quantity: availablePoke.quantity,
+          pokemon: {
+            id: availablePoke.pokemon.id,
+            name: availablePoke.pokemon.name,
+            type: availablePoke.pokemon.type,
+            weight: availablePoke.pokemon.weight,
+          },
+        };
+      }
+    );
+
+    return {
+      storeId: useCaseOutput.id,
+      availablePokemons,
+    };
+  }
 }

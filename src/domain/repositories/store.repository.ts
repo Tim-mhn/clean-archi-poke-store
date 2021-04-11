3e697be2b71dde8f51bed8f1fa41c004ea0e6e8c
AlexJerome <alexandra.jerome@student.ecp.fr>
2021-04-11 17:23:51 +0200

    fix(ShoppingCartUseCase): get Store infos in usecase

--- src/domain/repositories/store.repository.ts
+++ src/domain/repositories/store.repository.ts
@@ -1,8 +1,10 @@
-import { Service } from "typedi";
+import { Service } from 'typedi'
 
 @Service()
 export abstract class AbstractStoreRepository {
-  abstract getAvailablePokemonsFromStore(
-    storeId: string
-  ): Promise<{ quantity: number; id: string }[]>;
+    abstract getAvailablePokemonsFromStore(
+        storeId: string
+    ): Promise<{ quantity: number; id: string }[]>
+
+    abstract getStoreById(storeId: string): Promise<{ id; availablePokemons }>
 }
--- src/infrastructure/controllers/shoppingCartController.ts
+++ src/infrastructure/controllers/shoppingCartController.ts
@@ -1,100 +1,107 @@
-import "reflect-metadata"; // Don't forget to import this for each Controller
-import { Delete, Get, JsonController, Param, Post } from "routing-controllers";
-import { Inject, Service } from "typedi";
-import { AbstractPokemonRepository } from "../../domain/repositories/pokemon.repository";
-import { AbstractShoppingCartRepository } from "../../domain/repositories/shoppingCart.repository";
+import 'reflect-metadata' // Don't forget to import this for each Controller
 import {
-  AddPokemonToShoppingCartInput,
-  AddPokemonToShoppingCartUseCase,
-} from "../../domain/usecases/addPokemonToShoppingCart.useCase";
-import { 
-  CreateEmptyShoppingCartInput,
-  CreateEmptyShoppingCartUseCase 
-} from "../../domain/usecases/createEmptyShoppingCart.useCase";
-import { CreateEmptyShoppingCartPresenter } from "../presenters/createEmptyShoppingCart.presenter";
-import { PokemonRepositoryProxy } from "../repositoryProxies/pokemonRepository.proxy";
-import { ShoppingCartRepositoryProxy } from "../repositoryProxies/shoppingCartRepository.proxy";
-import bodyParser = require("body-parser");
-import { 
-  RemovePokemonFromShoppingCartInput,
-  RemovePokemonFromShoppingCartUseCase
- } from "../../domain/usecases/removePokemonFromShoppingCart.useCase";
+    Delete,
+    Get,
+    JsonController,
+    Param,
+    Post,
+    QueryParam,
+} from 'routing-controllers'
+import { Inject, Service } from 'typedi'
+import { AbstractPokemonRepository } from '../../domain/repositories/pokemon.repository'
+import { AbstractShoppingCartRepository } from '../../domain/repositories/shoppingCart.repository'
+import { AbstractStoreRepository } from '../../domain/repositories/store.repository'
+import {
+    AddPokemonToShoppingCartInput,
+    AddPokemonToShoppingCartUseCase,
+} from '../../domain/usecases/addPokemonToShoppingCart.useCase'
+import {
+    CreateEmptyShoppingCartInput,
+    CreateEmptyShoppingCartUseCase,
+} from '../../domain/usecases/createEmptyShoppingCart.useCase'
+import {
+    RemovePokemonFromShoppingCartInput,
+    RemovePokemonFromShoppingCartUseCase,
+} from '../../domain/usecases/removePokemonFromShoppingCart.useCase'
+import { CreateEmptyShoppingCartPresenter } from '../presenters/createEmptyShoppingCart.presenter'
+import { PokemonRepositoryProxy } from '../repositoryProxies/pokemonRepository.proxy'
+import { ShoppingCartRepositoryProxy } from '../repositoryProxies/shoppingCartRepository.proxy'
+import { StoreRepositoryProxy } from '../repositoryProxies/storeRepository.proxy'
+import bodyParser = require('body-parser')
 
-@JsonController("/shopping-cart")
+@JsonController('/shopping-cart')
 @Service()
 export class ShoppingCartController {
-  constructor(
-    @Inject(ShoppingCartRepositoryProxy.getInstance)
-    private readonly shoppingCartRepository: AbstractShoppingCartRepository,
-    @Inject(() => CreateEmptyShoppingCartPresenter)
-    @Inject(PokemonRepositoryProxy.getInstance)
-    private readonly pokemonRepository: AbstractPokemonRepository,
+    constructor(
+        @Inject(ShoppingCartRepositoryProxy.getInstance)
+        private readonly shoppingCartRepository: AbstractShoppingCartRepository,
+        @Inject(() => CreateEmptyShoppingCartPresenter)
+        @Inject(PokemonRepositoryProxy.getInstance)
+        private readonly pokemonRepository: AbstractPokemonRepository,
+        @Inject(StoreRepositoryProxy.getInstance)
+        private readonly storeRepository: AbstractStoreRepository,
 
-    private readonly createEmptyShoppingCartPresenter: CreateEmptyShoppingCartPresenter,
-    private readonly createEmptyShoppingCartUseCase: CreateEmptyShoppingCartUseCase,
-    private readonly addPokemonToShoppingCartUseCase: AddPokemonToShoppingCartUseCase,
-    private readonly removePokemonFromShoppingCartUseCase: RemovePokemonFromShoppingCartUseCase
-
-  ) {
-    this.createEmptyShoppingCartUseCase = new CreateEmptyShoppingCartUseCase(
-      shoppingCartRepository
-    );
-    this.createEmptyShoppingCartPresenter = createEmptyShoppingCartPresenter;
-    this.addPokemonToShoppingCartUseCase = new AddPokemonToShoppingCartUseCase(
-      shoppingCartRepository,
-      pokemonRepository
-    );
-    this.removePokemonFromShoppingCartUseCase = new RemovePokemonFromShoppingCartUseCase(
-      shoppingCartRepository,
-      pokemonRepository
-    );
-  }
-
-  @Post("/:storeId")
-  async createEmptyShoppingCart(
-    @Param("storeId") storeId: string
-  ) {
-
-    const useCaseInput : CreateEmptyShoppingCartInput = {
-      storeId: storeId
+        private readonly createEmptyShoppingCartPresenter: CreateEmptyShoppingCartPresenter,
+        private readonly createEmptyShoppingCartUseCase: CreateEmptyShoppingCartUseCase,
+        private readonly addPokemonToShoppingCartUseCase: AddPokemonToShoppingCartUseCase,
+        private readonly removePokemonFromShoppingCartUseCase: RemovePokemonFromShoppingCartUseCase
+    ) {
+        this.createEmptyShoppingCartUseCase = new CreateEmptyShoppingCartUseCase(
+            shoppingCartRepository,
+            storeRepository
+        )
+        this.createEmptyShoppingCartPresenter = createEmptyShoppingCartPresenter
+        this.addPokemonToShoppingCartUseCase = new AddPokemonToShoppingCartUseCase(
+            shoppingCartRepository,
+            pokemonRepository
+        )
+        this.removePokemonFromShoppingCartUseCase = new RemovePokemonFromShoppingCartUseCase(
+            shoppingCartRepository,
+            pokemonRepository
+        )
     }
-    const useCaseOutput = await this.createEmptyShoppingCartUseCase.execute(useCaseInput);
-    return this.createEmptyShoppingCartPresenter.present(useCaseOutput);
-  }
 
-  @Post("/:shoppingCartId/pokemon/:pokemonId")
-  async addPokemonToShoppingCart(
-    @Param("pokemonId") pokemonId: string,
-    @Param("shoppingCartId") shoppingCartId: string
-  ) {
-    const input: AddPokemonToShoppingCartInput = {
-      pokemonId: pokemonId,
-      shoppingCartId: shoppingCartId,
-    };
-    const useCaseOutput = await this.addPokemonToShoppingCartUseCase.execute(
-      input
-    );
-    return useCaseOutput;
-  }
+    @Post('/:shoppingCartId/pokemon/:pokemonId')
+    async addPokemonToShoppingCart(
+        @Param('pokemonId') pokemonId: string,
+        @Param('shoppingCartId') shoppingCartId: string
+    ) {
+        const input: AddPokemonToShoppingCartInput = {
+            pokemonId: pokemonId,
+            shoppingCartId: shoppingCartId,
+        }
+        const useCaseOutput = await this.addPokemonToShoppingCartUseCase.execute(
+            input
+        )
+        return useCaseOutput
+    }
 
-  @Delete("/:shoppingCartId/pokemon/:pokemonId")
-  async removePokemonFromShoppingCart(
-    @Param("pokemonId") pokemonId: string,
-    @Param("shoppingCartId") shoppingCartId: string
-  ) {
-    const input: RemovePokemonFromShoppingCartInput = {
-      pokemonId: pokemonId,
-      shoppingCartId: shoppingCartId,
-    };
-    const useCaseOutput = await this.removePokemonFromShoppingCartUseCase.execute(
-      input
-    );
-    return useCaseOutput;
-  }
+    @Delete('/:shoppingCartId/pokemon/:pokemonId')
+    async removePokemonFromShoppingCart(
+        @Param('pokemonId') pokemonId: string,
+        @Param('shoppingCartId') shoppingCartId: string
+    ) {
+        const input: RemovePokemonFromShoppingCartInput = {
+            pokemonId: pokemonId,
+            shoppingCartId: shoppingCartId,
+        }
+        const useCaseOutput = await this.removePokemonFromShoppingCartUseCase.execute(
+            input
+        )
+        return useCaseOutput
+    }
+    @Post()
+    async createEmptyShoppingCart(@QueryParam('storeId') storeId: string) {
+        console.log(storeId)
+        const useCaseInput: CreateEmptyShoppingCartInput = {
+            storeId: storeId,
+        }
+        const useCaseOutput = await this.createEmptyShoppingCartUseCase.execute(
+            useCaseInput
+        )
+        return this.createEmptyShoppingCartPresenter.present(useCaseOutput)
+    }
 
-  @Get("/:shoppingCartId")
-  async getShoppingCartDetails() {
-
-  }
-
+    @Get('/:shoppingCartId')
+    async getShoppingCartDetails() {}
 }
